7b8e3e7aa43e8f84ec8328af84d719cdfba2f2ce
Jericho Abuedo Alba <145337744+jerichoalbae@users.noreply.github.com>
2025-01-30 23:38:32 +0800

    Add a second component based on the first component

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,18 +1,18 @@
-<template>
-  <h1>Food</h1>
-  <food-item/>
-  <food-item/>
-  <food-item/>
-</template>
-
 <script></script>
 
-<style>
-  #app > div {
-    border: dashed black 1px;
-    display: inline-block;
-    margin: 10px;
-    padding: 10px;
-    background-color: lightgreen;
-  }
-</style>
+<template>
+   <h1>Food</h1>
+  <food-item/> <br>
+  <food-item2/> <br>
+  <food-item/>  <br>
+  <food-item2/> <br> 
+</template>
+
+<style></style>
+
+
+
+
+
+
+
