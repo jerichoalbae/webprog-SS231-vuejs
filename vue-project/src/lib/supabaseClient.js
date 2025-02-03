73332d5d5392e89d9f09da01be94c8da88efa7fc
Jericho Abuedo Alba <145337744+jerichoalbae@users.noreply.github.com>
2025-02-04 02:06:02 +0800

    gumana kana pls

--- vue-project/src/lib/supabaseClient.js
+++ vue-project/src/lib/supabaseClient.js
@@ -1,3 +1,3 @@
 import { createClient } from '@supabase/supabase-js'
 
-export const supabase = createClient('https://skiepwpylrbjtpphdnyf.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNraWVwd3B5bHJianRwcGhkbnlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNTQ2ODUsImV4cCI6MjA1MzgzMDY4NX0.Y1zplo4mtnXfgpcBBCJZlxhKs6Dtn6QcFLUWTNvbyQk')
+export const supabase = createClient('https://cazxhjkovppowqvuabid.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhenhoamtvdnBwb3dxdnVhYmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNTQ5ODgsImV4cCI6MjA1MzgzMDk4OH0.ETEdCfuWtOUNggWDqGQVYXRS1N1yKoU-Vwq-qEeIjHo')
