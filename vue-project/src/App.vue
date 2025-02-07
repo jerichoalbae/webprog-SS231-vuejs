1295ab37b3cd484cdce4c48ec242c9789ce3f4cd
Jericho Abuedo Alba <145337744+jerichoalbae@users.noreply.github.com>
2025-02-07 09:16:01 +0800

    ano ba gagana ka ba?

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,35 +1,14 @@
 <template>
-  <h1>Food</h1>
+<h1>Food</h1>
   <personal-profile /> <br />
   <food-item /> <br />
   <food-item2 /> <br />
   <food-item /> <br />
   <food-item2 /> <br />
-  <country /> <br />
-  <comment-form /> <br />  <comment />
+  <country/> <br/>
+  <comment-form/>
+  <comment/>
 </template>
 
-<script>
-import PersonalProfile from './components/PersonalProfile.vue'; // Adjust path
-import FoodItem from './components/FoodItem.vue'; // Adjust path
-import FoodItem2 from './components/FoodItem2.vue'; // Adjust path
-import Country from './components/Country.vue'; // Adjust path
-import CommentForm from './components/CommentForm.vue'; // Adjust path
-import Comment from './components/Comment.vue'; // Adjust path
-
-export default {
-  components: {
-    PersonalProfile,
-    FoodItem,
-    FoodItem2,
-    Country,
-    CommentForm,
-    Comment
-  },
-  // ... any other options for App.vue
-};
-</script>
-
 <style>
-/* Your App.vue styles */
 </style>
