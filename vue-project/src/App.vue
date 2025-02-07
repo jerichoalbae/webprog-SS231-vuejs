da6824c19b864a586095f9bfb6aad81b3d752d38
Jericho Abuedo Alba <145337744+jerichoalbae@users.noreply.github.com>
2025-02-07 08:45:31 +0800

    push

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,33 +1,14 @@
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from './lib/supabaseClient'
-
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
-</script>
-
 <template>
-  <ul>
-    <li v-for="country in countries" :key="country.id">
-      {{ country.name }}
-    </li>
-  </ul>
-
   <h1>Food</h1>
-  <personal-profile /> <br />
-  <food-item /> <br />
-  <food-item2 /> <br />
-  <food-item /> <br />
-  <food-item2 /> <br />
+  <country/>
+  <food-item/>
+  <food-item/>
+  <food-item/>
+  <comment-form/>
+  <comment/>
+   
+
 </template>
 
-<style>
-</style>
+<script></script>
+
