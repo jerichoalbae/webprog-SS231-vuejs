bd4253b481983f50ee3ddeaeb9ed0304b63d18b0
Jericho Abuedo Alba <145337744+jerichoalbae@users.noreply.github.com>
2025-03-03 14:24:53 +0800

    insert key

--- vue-project/src/components/CommentForm.vue
+++ vue-project/src/components/CommentForm.vue
@@ -41,7 +41,7 @@ export default {
       comment: '',
       submissionStatus: null,
       supabase: null,
-      supabaseUrl: 'https://cazxhjkovppowqvuabid.supabase.co', // Your Supabase URL - DEVELOPMENT ONLY
+      supabaseUrl: 'https://supabase.com/docs/guides/getting-started/quickstarts/vue', // Your Supabase URL - DEVELOPMENT ONLY
       supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhenhoamtvdnBwb3dxdnVhYmlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgyNTQ5ODgsImV4cCI6MjA1MzgzMDk4OH0.ETEdCfuWtOUNggWDqGQVYXRS1N1yKoU-Vwq-qEeIjHo', // Your Supabase Anon Key - DEVELOPMENT ONLY
       tableName: 'comments',
       comments:,
